b849354fe7afe090d00de71e1a6e901a0163148c
dikshitakambri <dikshitakambri18@gmail.com>
2021-03-03 20:39:38 +0530

    arrow function

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,18 +1,6 @@
 import React from "react";
-import emojipedia from "../emojipedia";
+import emojipedia from "../emojipedia.js";
 import Entry from "./Entry";
-
-function Dictionary(emoji) {
-  return (
-    <Entry
-      key={emoji.id}
-      aria-label={emoji.name}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
-  );
-}
 
 function App() {
   return (
@@ -21,7 +9,17 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <input className="input" />
-      <dl className="dictionary">{emojipedia.map(Dictionary)}</dl>
+      <dl className="dictionary">{emojipedia.map(emoji =>
+          <Entry
+            key={emoji.id}
+            aria-label={emoji.name}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+          />
+       )
+      }
+    </dl>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Component/App';
+import emojipedia from "./emojipedia";
+
+const newEmojipedia = emojipedia.map(function(emojiEntry){
+  return emojiEntry.meaning.substring(0, 100)
+});
+
+console.log(newEmojipedia);
 
 ReactDOM.render(
   <React.StrictMode>
