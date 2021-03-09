8a012b3c7d1516abc38e934fddd3345db4f1516b
dikshitakambri <dikshitakambri18@gmail.com>
2021-03-09 22:56:31 +0530

    react form -spread operaator

--- React form/src/Components/App.js
+++ React form/src/Components/App.js
@@ -1,13 +1,51 @@
 import React, { useState } from "react";
-import Form from "./Login";
-
-const isUserRegistered = true;
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handlechange(event) {
+    const newV = event.target.value;
+    const input = event.target.name;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [input]: newV
+      };
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <Form isRegister={isUserRegistered} />
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          name="fName"
+          onChange={handlechange}
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          name="lName"
+          onChange={handlechange}
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          onChange={handlechange}
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
