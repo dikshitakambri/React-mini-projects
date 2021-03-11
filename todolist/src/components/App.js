7cbbf14ae0e6919fe3ad972b245a3039a19cfb1b
dikshitakambri <dikshitakambri18@gmail.com>
2021-03-11 16:01:18 +0530

    todolist

---
+++ todolist/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function App() {
+  const [textItem, settextItem] = useState("");
+  const [item, setItem] = useState([]);
+
+  function Handlechange(event) {
+    const newItem = event.target.value;
+
+    settextItem(newItem);
+  }
+
+  function Additem() {
+    setItem((prevItem) => {
+      return [...prevItem, textItem];
+    });
+    settextItem("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" value={textItem} onChange={Handlechange} />
+        <button onClick={Additem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {item.map((todolist) => (
+            <li>{todolist}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
